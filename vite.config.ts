ee242973f69ca2ef0f3aa0893bed38bd64127dac
bynoty <bynot_noob@hotmail.com>
2026-07-11 21:11:40 +0700

    build: update base path for relative asset loading

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/Linkkeeper/',
+    base: './',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
